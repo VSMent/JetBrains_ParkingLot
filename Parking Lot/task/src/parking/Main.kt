9df849210c029ef8d50ed24138ddcf556213491a
Vasyl <vsmentvs@gmail.com>
2020-04-04 01:37:12 +0300

    Stage 2 completed

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,22 @@
 package parking
 
+import java.util.*
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val sc = Scanner(System.`in`)
+    var takenSlots = 1
+    val command = sc.next()
+    when (command) {
+        "park" -> {
+            sc.next()
+            print("${sc.next()} car parked on the spot ${takenSlots + 1}.")
+        }
+        "leave" -> {
+            val n = sc.nextInt()
+            print(
+                    if (n == takenSlots) "Spot $takenSlots is free."
+                    else "There is no car in the spot $n."
+            )
+        }
+    }
 }
